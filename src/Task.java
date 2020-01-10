79bfab2b245bf25061fb01d4e9e2804552bc3def
Michael George <georgem@wlwv.k12.or.us>
2020-01-10 09:40:39 -0800

    Added Priority, Slogan, SloganClient, and Task

---
+++ src/Task.java
@@ -0,0 +1,31 @@
+public class Task implements Priority {
+
+    private String name;
+    private int priority;
+    private boolean isComplete;
+
+    public Task(String name, int priority){
+        this.name = name;
+        isComplete = false;
+        this.priority = priority;
+    }
+
+    public void setPriority(int priorityLevel){
+        this.priority = priorityLevel;
+    }
+
+    public int getPriority(){
+        return priority;
+    }
+
+    public void doTask(){
+        System.out.println("Doing task...working...");
+        isComplete = true;
+        this.setPriority(0);
+    }
+
+    public String toString(){
+        return "Task: " + name + "\nPriority Level: " + this.getPriority();
+    }
+
+}
